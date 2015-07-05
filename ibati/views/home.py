32d14df7bcbfdb15e74942b785f92e498de3aacb
whypro <whypro@live.cn>
2015-07-05 22:56:54 +0800

    Modify styles of index

--- ibati/views/home.py
+++ ibati/views/home.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, abort, current_app
-from ibati.models import Slider, Category, Post
+from ibati.models import Slider, Category, Post, Label
 
 
 home = Blueprint('home', __name__)
@@ -10,15 +11,19 @@ home = Blueprint('home', __name__)
 def index():
     sliders = Slider.query.filter_by(enable=True).order_by(Slider.order.asc()).all()
 
-    cat = Category.query.filter_by(name='news').one()
-    news = Post.query.filter_by(category_id=cat.id).limit(current_app.config['INDEX_NEWS_NUM'])
-    return render_template('index.html', active='index', sliders=sliders, news=news)
+    research_cat = Category.query.filter_by(name='research').one()
+    area_label = Label.query.filter_by(name='area').one()
+    areas = Post.query.filter_by(category_id=research_cat.id, label_id=area_label.id).limit(current_app.config['INDEX_NEWS_NUM'])
+
+    news_cat = Category.query.filter_by(name='news').one()
+    dispatch_label = Label.query.filter_by(name='dispatch').one()
+    notice_label = Label.query.filter_by(name='notice').one()
+    report_label = Label.query.filter_by(name='report').one()
+    dispatches = Post.query.filter_by(category_id=news_cat.id, label_id=dispatch_label.id).limit(current_app.config['INDEX_NEWS_NUM'])
+    notices = Post.query.filter_by(category_id=news_cat.id, label_id=notice_label.id).limit(current_app.config['INDEX_NEWS_NUM'])
+    reports = Post.query.filter_by(category_id=news_cat.id, label_id=report_label.id).limit(current_app.config['INDEX_NEWS_NUM'])
+    return render_template('index.html', active='index', sliders=sliders, areas=areas, dispatches=dispatches, notices=notices, reports=reports)
     # return render_template('index.html')
-
-
-@home.route('/about-us/')
-def about_us():
-    return render_template('about-us.html', active='about_us')
 
 
 @home.route('/contact-us/')

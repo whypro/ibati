6e8ee119f221c4d3415c2bd2a435fe1cc343bbda
whypro <whypro@live.cn>
2015-07-10 23:35:24 +0800

    Add create database shell

--- manage.py
+++ manage.py
@@ -90,15 +90,34 @@ def restore():
 
 @manager.command
 def init():
+    # 创建数据库
+    create_db_sql = 'CREATE DATABASE IF NOT EXISTS {0} DEFAULT CHARACTER SET utf8'.format(config.Config.DB_DATABASE)
+    # print create_db_sql
+    ret = subprocess.call(
+        [
+            'mysql', '-u', config.Config.DB_USERNAME,
+            '-p{0}'.format(config.Config.DB_PASSWORD),
+            '-e', create_db_sql,
+        ]
+    )
+    if not ret:
+        print '数据库创建成功'
+    else:
+        print '数据库创建失败'
+        return 
+
     db.drop_all()
     db.create_all()
+    print '数据表创建成功'
     init_home(db.session)
     init_post(db.session)
     init_member(db.session)
+    print '数据初始化成功'
 
     uploads_dir = config.Config.UPLOADS_DEFAULT_DEST
     if os.path.exists(uploads_dir):
         shutil.rmtree(uploads_dir)
+    print '目录初始化成功'
 
 
 if __name__ == '__main__':
